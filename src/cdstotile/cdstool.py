6c20a11438ceaa940f9570c3381853f0ab901279
John A McInnes <s2@forceway.com>
2021-07-21 18:02:16 -0800

    download to a temporary file then rename

--- src/cdstotile/cdstool.py
+++ src/cdstotile/cdstool.py
@@ -1,11 +1,9 @@
 import os.path
 import cdsapi
+import netCDF4
 
 cds = cdsapi.Client()
-
-#cell_lat = 59.5
-#cell_long = -151.75
-app_version = "0.9"
+app_version = "0.91"
 force_download = False;
 
 # the variables we are interested in
@@ -21,36 +19,45 @@ variables = [
     'total_precipitation',
 ]
 
-# the years we want
+# the years we want. era5 back extension goes from 1950 to 1978
 era5_start_year = 1950
-era5_end_year = 1951#1978
+era5_end_year = 1955#1978
 years = list( range( era5_start_year, era5_end_year + 1 ) )
 
 # hello
 print( f'** ][WITW Copernicus data download tool v{app_version}**\n')
 
-for var_name in variables:
-    for year in years: 
+for year in years:
+    for var_name in variables:
         # file naming scheme
         pathname = f'./cds_era5_backext/{year}/'
         filename = f'global-{year}-{var_name}.nc'
         fullname = pathname + filename
 
         # see if file already downloaded.. if it exists and is larger then some nonsense amount
-        already_exists = os.path.isfile( fullname ) and os.path.getsize( fullname ) > 100
+        already_exists = os.path.isfile( fullname ) and os.path.getsize( fullname ) > 500
         if already_exists:
             print( f'{filename} exists already' )
+            # todo: validate the existing netcdf file
+            # # check if it is a valid complete download by trying to load it
+            # try:
+            #     ds = netCDF4.Dataset( fullname )
+            # except OSError:
+            #     print( f'{filename} is not a valid netcdf' )
+            #     already_exists = False;
 
         if not already_exists or force_download:
             print( f'{filename} requested...')
-            # remove any existing file & create the path
+            # remove any existing file
             try:
                 os.remove( fullname )
             except FileNotFoundError:
-                pass 
+                pass
+            # create the path if necessary
             os.makedirs( pathname, exist_ok=True )
 
-            # download to file
+            # download to temp file
+            tempfullname = fullname + '.tempdl' 
             r = cds.retrieve(
                 'reanalysis-era5-single-levels-preliminary-back-extension',
                 {
@@ -59,47 +66,8 @@ for var_name in variables:
                     'year': [year],
                     'time': 'all',
                     'variable': [ var_name ]
-                }, fullname )
+                }, tempfullname )
             #r.download()
+            # rename completed download
+            os.rename( tempfullname, fullname )
 
-
-
-
-
-
-
-# r = cds.retrieve(
-#     'reanalysis-era5-single-levels-preliminary-back-extension',
-#     {
-#         'product_type': 'reanalysis',
-#         'format': 'netcdf',
-#         'year': [1950],#list(range(1950, 1978 + 1)),
-#         #'month': '01',
-#         #'day': '01',
-#         'time': [
-#             '00:00', '01:00', '02:00',
-#             '03:00', '04:00', '05:00',
-#             '06:00', '07:00', '08:00',
-#             '09:00', '10:00', '11:00',
-#             '12:00', '13:00', '14:00',
-#             '15:00', '16:00', '17:00',
-#             '18:00', '19:00', '20:00',
-#             '21:00', '22:00', '23:00',
-#         ],
-#         'variable': [
-#             '10m_u_component_of_wind'
-#             # '10m_v_component_of_wind',
-#             # '2m_dewpoint_temperature',
-#             # '2m_temperature',
-#             # 'cloud_base_height',
-#             # 'precipitation_type',
-#             # 'surface_pressure',
-#             # 'total_cloud_cover',
-#             # 'total_precipitation',
-#         ],
-#         'area': [
-#             cell_lat+0.2499, cell_long+0.2499,
-#             cell_lat, cell_long,
-#         ],
-#     }, )
-#     #'download.nc')
