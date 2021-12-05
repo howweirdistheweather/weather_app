74a615539802ce5ee45c5762e05ec9fd960ee5e4
sloth1 <sloth1@bossel.lan>
2021-12-05 13:08:29 -0900

    :working on tiletool

--- src/cdstotile/hwxpo.py
+++ src/cdstotile/hwxpo.py
@@ -1,8 +1,19 @@
 # HWITW Copyright (C) 2021
 #
-# a class to contain processed output data
+# a class to contain HWITW processed output data
 
 import json
+import sys
+
+def sizeof(obj):
+    size = sys.getsizeof(obj)
+    if isinstance(obj, dict): return size + sum(map(sizeof, obj.keys())) + sum(map(sizeof, obj.values()))
+    if isinstance(obj, (list, tuple, set, frozenset)): return size + sum(map(sizeof, obj))
+    return size
+
+WEEKS_PER_YEAR = 52
+TOTAL_NUM_LAT  = 721        # number of 1/4 degree latitude entries expected
+TOTAL_NUM_LONG = 1440       # ..and 1/4 degree longitude entries
 
 # Names of Processed Outputs
 HWX_TEMP_AVG       = 'temperature_avg'
@@ -22,32 +33,68 @@ def dict_to_slist( src_dict:dict ):
     result = [src_dict[key] for key in sorted(src_dict.keys())]
     return result
 
+# class to contain all statistics for 1 week for 1 location
+class HWXStats:
+    
+    def __init__( self ):
+        self.temp_avg:float     = None
+        self.temp_avg_n:float   = None
+        self.temp_avg_d:float   = None
+        self.ceiling_avg:float  = None
+    
+    def merge( self, hstat:'HWXStats' ):
+        if hstat.temp_avg       != None: self.temp_avg      = hstat.temp_avg
+        if hstat.temp_avg_n     != None: self.temp_avg_n    = hstat.temp_avg_n
+        if hstat.temp_avg_d     != None: self.temp_avg_d    = hstat.temp_avg_d
+        if hstat.ceiling_avg    != None: self.ceiling_avg   = hstat.ceiling_avg
 
-class HWXPO:
+# class to contain weekly data for 1 year for 1 location
+class HWXWeeklyData:
 
     def __init__( self ):
+        self.Weeks = [HWXStats() for i in range(WEEKS_PER_YEAR)]
+
+    def set_week( self, week:int, hstat:HWXStats ):
+        self.Weeks[ week ].merge( hstat )
+
+
+# A class to contain all processed output statistics for a single location
+class HWXLocation:
+
+    def __init__( self ):
+        self.WXStats:list = []
         self.TempAvg:dict = {}
         self.TempAvgNight:dict = {}
         self.TempAvgDay:dict = {}
         self.CeilingAvg:dict = {}
-        pass    
+        pass
 
+    def hide__merge( self, hpo_in ):
+        self.TempAvg.update( hpo_in.TempAvg )
+        self.TempAvgNight.update( hpo_in.TempAvgNight )
+        self.TempAvgDay.update( hpo_in.TempAvgDay )
+        self.CeilingAvg.update( hpo_in.CeilingAvg )
 
-    def add_temp_avg( self, year:int, weekly_ta:list ):
-        self.TempAvg[year] = weekly_ta
-        
-    def add_temp_avg_n( self, year:int, weekly_tan:list ):
-        self.TempAvgNight[year] = weekly_tan
-        
-    def add_temp_avg_d( self, year:int, weekly_tad:list ):
-        self.TempAvgDay[year] = weekly_tad
+    @staticmethod
+    def set_weekly( d_y:dict, year:int, week:int, x:float ):        
+        if x is None:
+            return
+        if year not in d_y.keys():
+            d_y[year] = HWXWeeklyData()
+            
+        d_y[year].set_week( week, x )
 
-    def add_ceiling_avg( self, year:int, weekly_ca:list ):
-        self.CeilingAvg[year] = weekly_ca
-    
+    def set( self, year:int, week:int, hstat:HWXStats ):
+        HWXLocation.set_weekly( self.TempAvg,      year, week, hstat.temp_avg )
+        HWXLocation.set_weekly( self.TempAvgNight, year, week, hstat.temp_avg_n )
+        HWXLocation.set_weekly( self.TempAvgDay,   year, week, hstat.temp_avg_d )
+        HWXLocation.set_weekly( self.CeilingAvg,   year, week, hstat.ceiling_avg )
+
     # put everything in a JSON-able dictionary and return it
-    def get_jodict( self ):
-        # get start year from tempavg. assume its the same for all!!?
+    def get_jsondict( self ):
+        # get start year from tempavg. assuming it is the same for all statistics
+        # ..and that years are contiguous.
+        assert len(self.TempAvg) > 0
         start_year = sorted(self.TempAvg.keys())[0]
 
         # convert weather variable data to 'python list of lists'
@@ -57,7 +104,7 @@ class HWXPO:
         ceiling_avg = dict_to_slist( self.CeilingAvg )
         
         # put it together like this for the purpose of doing some json output
-        jodict = {
+        jdict = {
             'data_specs':{
                 'start_year':start_year
             },
@@ -72,7 +119,20 @@ class HWXPO:
                 }
             }
         }
+
+        return jdict
+
+
+# A class to contain array of hwx processed output for whole planet for 1 year
+class HWXGlobal:
+
+    def __init__( self, year:int ):
+        self.Year = year
+        self.LocArray = [[HWXWeeklyData() for i in range(TOTAL_NUM_LONG)] for j in range( TOTAL_NUM_LAT )]
+
+    def merge_stat( self, hstat:HWXStats, lat_idx:int, long_idx:int, week_idx:int ):
+        # merge one-week-one-location into array
+        self.LocArray[ lat_idx ][ long_idx ].set_week( week_idx, hstat )
         
-        return jodict
-        
-        
+    def merge( self, hg:'HWXGlobal' ):
+        print( 'nothing here yet!' )
--- src/cdstotile/tiletool.py
+++ src/cdstotile/tiletool.py
@@ -7,8 +7,18 @@ import os.path
 import json
 import numpy
 import netCDF4
+import timeit
+
+import parsl
+from parsl import python_app
+from parsl.config import Config
+#from parsl.executors.threads import ThreadPoolExecutor
+from parsl.providers import LocalProvider
+from parsl.channels import LocalChannel
+from parsl.executors import HighThroughputExecutor
 
 import hwxpo
+from wxstat import *
 
 # a helper class for printing color text
 class bcolors:
@@ -31,131 +41,60 @@ WEEKS_PER_YEAR = 52
 APP_VERSION = "0.50"
 current_time = datetime.datetime.now()
 
-#kelvin to fahrenheit
-def kelvin_to_F( temp_k:float ):
-    temp_f = (temp_k - 273.15) * 9/5 + 32
-    return temp_f
 
-#kelvin list to fahrenheit
-def kelvin_to_F( temp_ka:list ):
-    # use numpy for speed
-    # NULL/NoneType will become NaN
-    temp_np = numpy.asarray( temp_ka, dtype=float )
-    temp_fa = (temp_np - 273.15) * 9/5 + 32
-    # convert back to a list with NaN back to None.. we'll be outputting JSON which doesn't like NaN.
-    temp_fa2 = numpy.where( numpy.isnan(temp_fa), None, temp_fa )
-    return temp_fa2.tolist()
+def process_netcdf( fullname:str, filename:str, year:int, short_var_name:str ) -> hwxpo.HWXGlobal:
+    # load file and process each location and time
+    print( f'processing {filename}', flush=True )
+    try:
+        ds = netCDF4.Dataset( fullname, 'r' )
+    except OSError:
+        print( bcolors.FAIL + f'{filename} could not be opened!' + bcolors.ENDC )
+        exit(-1)
 
-#meter list to feet
-def meter_to_ft( dist_ma:list ):
-    # use numpy for speed
-    # NULL/NoneType will become NaN
-    dist_np = numpy.asarray( dist_ma, dtype=float )
-    dist_fta = (dist_np * 3.2808399)
-    # convert back to a list with NaN back to None.. we'll be outputting JSON which doesn't like NaN.
-    dist_fta2 = numpy.where( numpy.isnan(dist_fta), None, dist_fta )
-    return dist_fta2.tolist()
+    total_num_hours = ds.dimensions['time'].size
+    num_lat         = ds.dimensions['latitude'].size
+    num_long        = ds.dimensions['longitude'].size
+    assert total_num_hours >= 8753  #HOURS_PER_YEAR ...whats up with this 8753 hours?
+    assert num_long == 1440
+    assert num_lat == 721
 
-# flatten, reshape, rearrange out netcdf dataset variable data into a 2D array
-# pass: netcdf dataset, short var name present in the dataset
-def reshape_nds( ds:netCDF4.Dataset, short_vn:str, lat_ds:int, long_ds:int ):
-    # the netcdf array is a 3 dim array [time][lat][long]
-    # lat and long are always index 0 (assuming our cdf files have only one grid cell present)
-    # 
-    # get the variable of interest, we will call it 'x'
-    # ..flatten the array, truncate it to 52 weeks, and reshape it.
-    flat_x = ds[short_vn][0:HOURS_PER_YEAR,0,0]
-    flat_x = numpy.ma.resize( flat_x, WEEKS_PER_YEAR * HOURS_PER_WEEK )
-    array_x = numpy.reshape( flat_x, ( WEEKS_PER_YEAR, HOURS_PER_WEEK ))
-    return array_x  # array is now [week][hour]
+    hwxout = []# hwxpo.HWXGlobal( year )
 
-# calculate weekly average of a variable
-# pass: netcdf dataset, short var name present in the dataset
-# returns: list
-def do_hwxpo_avg( array_x):
-    weekly_results = [0.0] * 52
-    for week_num in range( 0, 52 ):
-        avg_x = numpy.average( array_x[ week_num ] )
-        weekly_results[ week_num ] = avg_x.tolist() # convert to list
+    # for each week in ds..
+    num_weeks = int(total_num_hours / HOURS_PER_WEEK)
+    for week_i in range( num_weeks ):
+        # print progress
+        print( f'\rweek {week_i}/{num_weeks}', end='\n', flush=True )
 
-    return weekly_results
+        # load the weeks worth of hours for all locations. transpose to long,lat,hour
+        hr_0 = week_i * HOURS_PER_WEEK
+        hr_1 = hr_0 + HOURS_PER_WEEK
+        var_week = numpy.transpose( ds[short_var_name][hr_0:hr_1][:][:] )
+        assert( var_week.dtype == numpy.float64 )
 
-# calculate weekly-NIGHTLY average of a variable
-# pass: netcdf dataset, short var name present in the dataset
-# returns: list
-def do_hwxpo_avg_night( array_x ):
-    # todo: adjust for local time
-    # a simplistic definition of 'night'? 18:00 to 06:00
-    weekly_results = [0.0] * 52
-    for week_num in range( 0, 52 ):
-        night_array_x = numpy.empty(0);
-        for day_num in range( 0, 7 ):
-            hrd = day_num * 24
-            hr0 = 0 + hrd       # early morning
-            hr1 = 6 + hrd
-            hr2 = 18 + hrd      # late night
-            hr3 = 24 + hrd
-            night_array_x = numpy.append( night_array_x, array_x[week_num][hr0:hr1] )
-            night_array_x = numpy.append( night_array_x, array_x[week_num][hr2:hr3] )
+        # for each location in var_week
+        for lat_i in range( num_lat ):
             
-        avg_x = numpy.average( night_array_x )
-        weekly_results[ week_num ] = avg_x
+            print( f'\r{lat_i}/{num_lat}', end='', flush=True )
+            for long_i in range( num_long ):
+                # get hourly as ndarray for this location
+                var_loc = numpy.ma.getdata( var_week[long_i][lat_i] )
+                #print( f'{type(var_loc)} {var_loc.dtype} {var_loc.shape} {var_loc.size}' )
+                #print( timeit.timeit( lambda: numpy.average( var_loc ), number=4 )/4 )                
+                # process a specific variable and week and location
+                hs = calc_week_stat( var_loc, short_var_name )
+                #hwxout.merge_stat( hs, lat_i, long_i, week_i )
 
-    return weekly_results
+    print( f'\r', end='', flush=True )    # newline
+    return hwxout
 
-# calculate weekly-DAYTIME average of a variable
-# pass: netcdf dataset, short var name present in the dataset
-# returns: list
-def do_hwxpo_avg_day( array_x ):    
-    # todo: adjust for local time
-    # a simplistic definition of 'day'? 06:00 to 18:00
-    weekly_results = [0.0] * 52
-    for week_num in range( 0, 52 ):
-        day_array_x = numpy.empty(0);
-        for day_num in range( 0, 7 ):
-            hrd = day_num * 24
-            hr0 = 6 + hrd
-            hr1 = 18 + hrd
-            day_array_x = numpy.append( day_array_x, array_x[week_num][hr0:hr1] )
-            
-        avg_x = numpy.average( day_array_x )
-        weekly_results[ week_num ] = avg_x
-
-    return weekly_results
-
-def do_hwxpo( year:int, short_var_name:str, array_x:numpy.array, hpo:hwxpo.HWXPO ):
-    # process the netcdf data
-    if short_var_name == CDSVAR_T2M[1]:
-        temp_avg = do_hwxpo_avg( array_x )
-        temp_avg = kelvin_to_F( temp_avg )
-        hpo.add_temp_avg( year, temp_avg )                
-        
-        temp_avg_n = do_hwxpo_avg_night( array_x )
-        temp_avg_n = kelvin_to_F( temp_avg_n )
-        hpo.add_temp_avg_n( year, temp_avg_n )
-
-        temp_avg_d = do_hwxpo_avg_day( array_x )
-        temp_avg_d = kelvin_to_F( temp_avg_d )
-        hpo.add_temp_avg_d( year, temp_avg_d )
-
-    elif short_var_name == CDSVAR_CBH[1]:
-        ceiling_avg = do_hwxpo_avg( array_x )
-        ceiling_avg = meter_to_ft( ceiling_avg )
-        hpo.add_ceiling_avg( year, ceiling_avg )
-
-    else:
-        return
-        print( bcolors.WARNING + f'Unhandled variable type! {short_var_name}' + bcolors.ENDC )
-
-    pass
-
-def load_netcdfs( hpo:hwxpo.HWXPO, dir_name, start_year, end_year, area_lat_long ):
+def load_netcdfs( dir_name, start_year, end_year ):
     # calculate a unique number for each quarter degree on the planet.
     # makes having a unique filename for the coordinates simpler.
-    grid_num = CalcQtrDegGridNum( area_lat_long )
+    #grid_num = CalcQtrDegGridNum( area_lat_long )
 
     years = list( range( start_year, end_year + 1 ) )
-    yearly_results = [None] * len(years)
+    #yearly_results = [None] * len(years)
 
     for year in years:
         yidx = year - start_year
@@ -166,42 +105,24 @@ def load_netcdfs( hpo:hwxpo.HWXPO, dir_name, start_year, end_year, area_lat_long
 
             # file naming scheme
             pathname = f'./{dir_name}/{year}/'
-            #filename = f'gn{grid_num}-{year}-{var_name}.nc'
+            # filename = f'gn{grid_num}-{year}-{var_name}.nc'
             filename = f'global-{year}-{var_name}.nc'
             fullname = pathname + filename
 
-            # make sure file already exists and load it
-            already_exists = os.path.isfile( fullname ) #and os.path.getsize( fullname ) > 500
+            # make sure file exists
+            already_exists = os.path.isfile( fullname )
             if not already_exists:
-                print( bcolors.WARNING + f'{filename} is missing!' + bcolors.ENDC )
+                print(bcolors.WARNING + f'{filename} is missing!' + bcolors.ENDC, flush=True)
                 continue
-            
-            # NOTE: dataset should have 3 dimensions. time as hour, a single lat, a single long
-            print( f'processing {filename}' )
-            try:
-                ds = netCDF4.Dataset( fullname )
-            except OSError:
-                print( bcolors.FAIL + f'{filename} could not be opened!' + bcolors.ENDC )
-                exit(-1)
-  
-            total_num_hours = ds.dimensions['time'].size
-            num_lat = ds.dimensions['latitude'].size
-            num_long = ds.dimensions['longitude'].size
-            assert total_num_hours >= 8753  #HOURS_PER_YEAR  ...whats up with this 8753 hours?
-            assert num_long == 1440
-            assert num_lat == 721
-            
-            # for each location in ds
-            for lat_ds in range( num_lat ):
-                print( lat_ds )
-                for long_ds in range( num_long ):
-                    array_x = reshape_nds( ds, short_var_name, lat_ds, long_ds )                    
-                    do_hwxpo( year, short_var_name, array_x, hpo )
-  
+
+            # process the file
+            hwx_f = process_netcdf( fullname, filename, year, short_var_name );
+            #write to disk
+
 def CalcQtrDegGridNum( area_lat_long ):
     grid_num = int( ((area_lat_long[0] + 90) * 4) * 360 * 4 + ((area_lat_long[1] + 180) * 4) )
     return grid_num
-    
+
 ##########################################################
 # main
 
@@ -217,62 +138,19 @@ long0 = math.floor( inp_long * 4 ) / 4
 long1 = (math.ceil( inp_long * 4 ) / 4) #- 0.01
 
 area0 = [ lat0, long0, lat1, long1 ]
-
-# the variables we are interested, long name and short name.
-# short name is used inside the netcdf files
-CDSVAR_U10 =    ['10m_u_component_of_wind', 'u10' ]
-CDSVAR_V10 =    ['10m_v_component_of_wind', 'v10' ]
-CDSVAR_D2M =    ['2m_dewpoint_temperature', 'd2m' ]
-CDSVAR_T2M =    ['2m_temperature',          't2m' ]
-CDSVAR_CBH =    ['cloud_base_height',       'cbh' ]
-CDSVAR_PTYPE =  ['precipitation_type',      'ptype' ]
-#CDSVAR_SP =     ['surface_pressure',        'sp' ]
-CDSVAR_TCC =    ['total_cloud_cover',       'tcc' ]
-CDSVAR_TP =     ['total_precipitation',     'tp' ]
-
-cds_variables = [
-    CDSVAR_U10,
-    CDSVAR_V10,
-    CDSVAR_D2M,
-    CDSVAR_T2M,
-    CDSVAR_CBH,
-    CDSVAR_PTYPE,
-#    CDSVAR_SP,
-    CDSVAR_TCC,
-    CDSVAR_TP
-]
-
-# load and process netcdf files for this area0
-hpo = hwxpo.HWXPO()
+area0 = None
 
 # era5 goes from 1979 to present
-load_netcdfs(   hpo,
-                'cds_era5',
-                1979, current_time.year,
-                area0 )
+load_netcdfs(   'cds_era5',
+                1979, current_time.year )
 
 # era5 back extension goes from 1950 to 1978
-load_netcdfs(   hpo,
-                'cds_era5_backext',
-                1950, 1978,
-                area0 )
+load_netcdfs(   'cds_era5_backext',
+                1950, 1978 )
 
 # dump json
-gnum = CalcQtrDegGridNum( area0 )
-outname = f'gn{gnum}-hwxpo.json'
-print( f'Output {outname}' )
-with open( outname, 'w') as outfile:
-    json.dump( hpo.get_jodict(), outfile )
-
-# temporary: dump temp average to a json text file
 #gnum = CalcQtrDegGridNum( area0 )
-#outname = f'gn{gnum}-temp_avg.json'
+#outname = f'gn{gnum}-hwxpo.json'
 #print( f'Output {outname}' )
 #with open( outname, 'w') as outfile:
-#    json.dump( hpo.TempAvg, outfile )
-
-# dump ceiling avg to json text
-#outname = f'gn{gnum}-ceiling_avg.json'
-#print( f'Output {outname}' )
-#with open( outname, 'w') as outfile:
-#    json.dump( hpo.CeilingAvg, outfile )
+    #json.dump( hpo.get_jodict(), outfile )
