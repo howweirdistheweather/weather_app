1208ffa9666bede0de10c7b7d61495cfa0b4f8c6
John A McInnes <jamcinnes2@gmail.com>
2022-05-05 20:56:19 -0800

    cleanup cdstool

--- src/cdstotile/cdstool.py
+++ src/cdstotile/cdstool.py
@@ -35,17 +35,13 @@ def download_dataset( ds_name, dir_name, start_year, end_year, area_lat_long, va
     '''
     assert start_year <= end_year
     years = list( range( start_year, end_year + 1 ) )
-    # # calculate a unique number for each quarter degree on the planet.
-    # # makes having a unique filename for the coordinates simpler.
-    grid_num = None
-    if area_lat_long != None:
-        grid_num = int( ((area_lat_long[0] + 90) * 4) * 360 * 4 + ((area_lat_long[1] + 180) * 4) )
-    print( f'Downloading {ds_name} {area_lat_long} (gn{grid_num}) from {start_year} to {end_year}')
+
+    print( f'Downloading {ds_name} {area_lat_long} from {start_year} to {end_year}')
 
     # download year, variable for entire globe
     for year in reversed( years ):
         for var_name in variables:
-            download_var_for_year( ds_name, dir_name, year, area_lat_long, var_name, grid_num, force_download)
+            download_var_for_year( ds_name, dir_name, year, area_lat_long, var_name, force_download)
             #print_var_for_year( ds_name, dir_name, year, area_lat_long, var_name, force_download)
             print(f'Submitted {var_name} for {year}: {ds_name}')
 
@@ -57,7 +53,7 @@ def print_var_for_year(ds_name, dir_name, year, area_lat_long, var_name, force_d
     print(f'Processing {var_name} for {year}: {ds_name}')
 
 @python_app
-def download_var_for_year(ds_name, dir_name, year, area_lat_long, var_name, grid_num, force_download=False):
+def download_var_for_year(ds_name, dir_name, year, area_lat_long, var_name, force_download=False):
     '''
     Download a single year dataset for a single variable.
     :param ds_name: The dataset name to be downloaded from CDS
@@ -78,7 +74,6 @@ def download_var_for_year(ds_name, dir_name, year, area_lat_long, var_name, grid
 
     # file naming scheme
     pathname = f'./{dir_name}/{year}/'
-    #filename = f'gn{grid_num}-{year}-{var_name}.nc'
     filename = f'global-{year}-{var_name}.nc'
     fullname = pathname + filename
 
@@ -133,7 +128,7 @@ def main():
     # hello
     print( f'** HWITW Copernicus data download tool v{app_version} **\n')
 
-    # 0.25 degree resolution
+    # 0.25 degree resolution. N positive, W negative
     #inp_lat = 59.64 # homer ak
     #inp_long = -151.54
     #inp_lat = 59.45 # Seldovia
@@ -158,7 +153,7 @@ def main():
     #area0 = [ lat0, long0, lat1, long1 ]
     area0 = None  # we are doing global downloads!
 
-    # the variables we are interested in
+    # the era5 variables we are interested in
     variables = [
         '10m_u_component_of_wind',
         '10m_v_component_of_wind',
