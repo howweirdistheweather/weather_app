0e32d03dba63ed7785c6fa1c8e06d7212944acbf
Bretwood Higman <Hig@Higs-MBP-2.lan>
2022-09-03 01:06:21 -0800

    Pruned down to the active code

--- src/website/run_local.sh
+++ src/website/run_local.sh
@@ -2,5 +2,4 @@
 
 export FLASK_APP=webapp.py
 export FLASK_ENV=development
-#sudo /home/duser/anaconda3/envs/hwitw/bin/flask run --host=0.0.0.0 --port=88
-flask run --host=0.0.0.0 --port=5000
+flask run --host=0.0.0.0 --port=5001
--- src/website2/run_local.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-export FLASK_APP=webapp.py
-export FLASK_ENV=development
-flask run --host=0.0.0.0 --port=5000
