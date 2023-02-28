3c29575bb678e5c9197529c8f50b75075bd0bbeb
Matt Jones <gitcode@magisa.org>
2023-02-28 01:04:11 -0500

    Use env vars to set script location and start year.
    Includes update of helm deploy to use these.

--- src/cdstotile/hwitw_update.sh
+++ src/cdstotile/hwitw_update.sh
@@ -5,18 +5,16 @@
 # Set these environment variables to specify where to download and where
 # to process data files.
 # (some tricky bash to give variable a default value if not set)
-: "${CDS_DOWNLOAD_DIR:='./'}"
-: "${DATA_OUTPUT_DIR:='./'}"
+: ${CDS_DOWNLOAD_DIR:='.'}
+: ${DATA_OUTPUT_DIR:='.'}
+: ${CDSTOOL_DIR:=.}
+
+# Start from the current year, or from $CDS_START_YEAR if it is already set
+: ${CDS_START_YEAR:=$(date +%Y)}
 
 set -e
-# Download the latest Copernicus CDS data.
-# ...It is currently 2023 - so we start from there. What happens in 2024+?
-# There is a small overhead as the tool checks that is has downloaded all
-# data for 2023, 2024, etc. Say < 1 minute total. It is harmless.
-python3 cdstool.py --startyear 2023 --output "$CDS_DOWNLOAD_DIR"
+# Download the latest Copernicus CDS data from $CDS_START_YEAR forward.
+python3 ${CDSTOOL_DIR}/cdstool.py --startyear ${CDS_START_YEAR} --output "$CDS_DOWNLOAD_DIR"
 
 # Process the data into our custom netcdfs and then into the wxdb
-# ...Same as above, there is a small overhead in following years as the
-# tool checks that it has processed all data for 2023, 2024, etc.
-python3 tiletool.py --start 2023 --update --input "$CDS_DOWNLOAD_DIR" --output "$DATA_OUTPUT_DIR"
-
+python3 ${CDSTOOL_DIR}/tiletool.py --start ${CDS_START_YEAR} --update --input "$CDS_DOWNLOAD_DIR" --output "$DATA_OUTPUT_DIR"
